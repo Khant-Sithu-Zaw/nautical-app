4a4757471ea7557178920717e77c3690b4b85b2d
Khant Sithu Zaw <98268896+Khant-Sithu-Zaw@users.noreply.github.com>
2025-10-22 21:10:33 +0630

    in development learn feature

--- src/screens/LearnScreen.js
+++ src/screens/LearnScreen.js
@@ -4,7 +4,7 @@ import styles from "../style/styles";
 import { scale, moderateScale, verticalScale } from '../utils/scale';
 export default function LearnScreen() {
     return (
-        <View style={styles.container}><Text style={{ fontSize: moderateScale(20), color: "#fff" }}>Learning Feature In Development</Text>
+        <View style={styles.container}><Text style={{ fontSize: moderateScale(40), color: "#fff", }}> {"Learning Feature\n In Development"}</Text>
         </View>
     )
 }
